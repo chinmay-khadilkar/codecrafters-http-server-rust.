5dcdd8d35e4d5919011c1670f0916fb0fa4ddc62
chinmay <chinmay.khadilkar@gmail.com>
2024-11-29 23:20:54 +0530

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[allow(unused_imports)]
 use std::net::{ TcpStream, TcpListener};
-use std::io::{ Write, BufReader, BufRead };
+use std::io::{ Write, BufReader, BufRead, Read };
 use std::{env, fs};
 use std::path::Path;
 use std::fs::File;
@@ -24,7 +24,7 @@ fn main() {
          match stream {
              Ok(stream) => {
                  println!("accepted new connection");
-                 std::thread::spawn(|| process_stream(stream));
+                 process_stream(stream);
              }
              Err(e) => {
                  println!("error: {}", e);
@@ -34,10 +34,17 @@ fn main() {
 }
 
 fn handle_connection (stream: &mut TcpStream) -> StatusCode {
-    let buffer = BufReader::new(stream);
-    let http_request: Vec<String> = buffer.lines().map(|line| line.unwrap()).collect();
-    let request_line: Vec<String> = http_request[0].split(" ").map(|item| item.to_string()).collect();
-    println!("{:?}", request_line);
+    let mut buffer = BufReader::new(stream);
+    let mut entire_request = String::new();
+    buffer.read_to_string(&mut entire_request);
+    
+    let req_vec:Vec<String> = entire_request.split("\r\n").map(|item| item.to_string()).collect();
+    println!("{:?}", req_vec);
+    // let http_request: Vec<String> = buffer.lines().map(|line| line.unwrap()).collect();
+    let request_line: Vec<String> = req_vec[0].split(" ").map(|item| item.to_string()).collect();
+    // let empty_pos = req_vec.iter().position(|item| item == String::from(""));
+    let content_body = req_vec[req_vec.len() - 1];
+    println!("{:?}", http_request);
 
     if request_line[0].starts_with("POST") {
         let content:Vec<String> = request_line[1].split("/").map(|item| item.to_string()).collect();
@@ -47,10 +54,9 @@ fn handle_connection (stream: &mut TcpStream) -> StatusCode {
         let file_path = Path::new(&dir).join(file_name);
         let prefix = file_path.parent().unwrap();
         std::fs::create_dir_all(prefix).unwrap();
-        let content = http_request[http_request.len() - 1].clone();
         let mut f = File::create(&file_path).unwrap();
-        f.write_all(content.as_bytes()).unwrap();
-        println!("{:?}", content);
+        f.write_all(content_body.as_bytes()).unwrap();
+        println!("{:?}", content_body);
         StatusCode::Created
         
     } else if request_line[1] == "/" {
@@ -115,5 +121,6 @@ fn process_stream (mut stream: TcpStream) {
             stream.write("HTTP/1.1 201 Created\r\n\r\n".as_bytes()).unwrap();
         }
     }
+    println!("Writing response to stream...");
     stream.flush().unwrap();
 }
